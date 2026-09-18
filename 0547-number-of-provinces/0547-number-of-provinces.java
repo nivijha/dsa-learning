bd240f90bdfb92ac2f25b6fe600984ac42beeb55
Nivi Jha <nivijha04@gmail.com>
2026-09-18 19:24:08 +0530

    Time: 1 ms (85.57%), Space: 47.1 MB (87.58%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void dfs(int node, boolean vis[], int[][] isConnected){
+        vis[node] = true;
+
+        for(int i=0; i<isConnected[node].length; i++){
+            if (isConnected[node][i] == 1 && !vis[i]) {
+                dfs(i, vis, isConnected);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        boolean vis[] = new boolean[isConnected.length];
+        int p = 0;
+
+        for (int i = 0; i < isConnected.length; i++) {
+
+            if (!vis[i]) {
+                dfs(i, vis, isConnected);
+                p++;
+            }
+        }
+
+        return p;
+    }
+}
